6062c60fda77063f48f7dfb3d65b2c4eb3d98518
d-e-k-k <davideberhart8@gmail.com>
2020-12-14 12:51:39 -0500

    Created temp dropdown toggle

--- src/components/aquarium_status/temperature/TemperatureList.js
+++ src/components/aquarium_status/temperature/TemperatureList.js
@@ -3,33 +3,49 @@ import axios from 'axios';
 import datepicker from 'js-datepicker';
 
 const TemperatureList = () => {
-    const [temperatureData, setTemperatureData] = useState()
-    const [endDate, setEndDate] = useState()
-    const[startDate, setStartDate] = useState()
-    // const url = 'https://mighty-lake-45709.herokuapp.com/temperatures';
-    const url = "./temp_seeds.json"
+	const [temperatureData, setTemperatureData] = useState();
+	const [endDate, setEndDate] = useState();
+	const [startDate, setStartDate] = useState();
+	const [showTemperatures, setShowTemperatures] = useState(false);
+	// const url = 'https://mighty-lake-45709.herokuapp.com/temperatures';
+	const url = './temp_seeds.json';
 	useEffect(() => {
 		axios({
 			method: 'GET',
 			url: url,
-		}).then((res) => {
-            setTemperatureData(res.data)
-        })
-        .catch(console.error)
+		})
+			.then((res) => {
+				setTemperatureData(res.data);
+			})
+			.catch(console.error);
 	}, []);
 
-	return <div>
-        <ul>
-           {temperatureData ?
-                temperatureData.map(temperatureObj => {
-                    return (
-                    <li>Date: {temperatureObj.date} Time: {temperatureObj.time} Temp F: {temperatureObj.temperature.toFixed(2)}</li>
-                        )
-                })
-                : null
-            }
-        </ul>
-    </div>;
+	function toggleShowTemperatures() {
+		showTemperatures ? setShowTemperatures(false) : setShowTemperatures(true);
+	}
+	return (
+		<div>
+			<h3 onClick={toggleShowTemperatures}>Temperatures</h3>
+			{showTemperatures ? (
+				<div>
+					<ul>
+						{temperatureData
+							? temperatureData.map((temperatureObj) => {
+									return (
+										<li>
+											Date: {temperatureObj.date} Time: {temperatureObj.time}{' '}
+											Temp F: {temperatureObj.temperature.toFixed(2)}
+										</li>
+									);
+							  })
+							: null}
+					</ul>
+				</div>
+			) : (
+				null
+			)}
+		</div>
+	);
 };
 
 export default TemperatureList;
