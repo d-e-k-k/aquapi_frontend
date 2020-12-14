0bcfa71527352b0807d19ba873a8447fdf69573b
d-e-k-k <davideberhart8@gmail.com>
2020-12-14 10:23:43 -0500

    Added aquarium status component with basic sub components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import NavBar from './components/navbar/NavBar';
 import Login from './components/login/Login';
+import AquariumStatus from './components/aquarium_status/AquariumStatus'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Login />
+      <AquariumStatus />
     </div>
   );
 }
