9c9bef4c9387529cd460ff1735ff152740d184b0
d-e-k-k <davideberhart8@gmail.com>
2020-12-14 11:51:11 -0500

    Mapped temp data to show all temps, dates, and times

--- src/components/aquarium_status/AquariumStatus.js
+++ src/components/aquarium_status/AquariumStatus.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import BasicInfo from './basic_info/BasicInfo'
 import EquipmentInfo  from './equipment_info/EquipmentInfo'
 import FishInfo from './fish_info/FishInfo'
+import TemperatureList from './temperature/TemperatureList'
+import axios from 'axios'
+
 
 const AquariumStatus = () => {
+
+
 	return (
 		<div>
 			<h2>D-e-k-k's Tank</h2>
@@ -11,6 +16,7 @@ const AquariumStatus = () => {
 				<BasicInfo />
 				<EquipmentInfo />
                 <FishInfo />
+                <TemperatureList />
 			</div>
 		</div>
 	);
---
+++ src/components/aquarium_status/temperature/TemperatureList.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+
+const TemperatureList = () => {
+    const [temperatureData, setTemperatureData] = useState()
+    // const url = 'https://mighty-lake-45709.herokuapp.com/temperatures';
+    const url = "./temp_seeds.json"
+	useEffect(() => {
+		axios({
+			method: 'GET',
+			url: url,
+		}).then((res) => {
+            setTemperatureData(res.data)
+        })
+        .catch(console.error)
+	}, []);
+
+	return <div>
+        <ul>
+           {temperatureData ?
+                temperatureData.map(temperatureObj => {
+                    return (
+                    <li>Date: {temperatureObj.date} Time: {temperatureObj.time} Temp F: {temperatureObj.temperature.toFixed(2)}</li>
+                        )
+                })
+                : null
+            }
+        </ul>
+    </div>;
+};
+
+export default TemperatureList;
